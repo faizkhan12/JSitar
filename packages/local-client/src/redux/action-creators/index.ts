e6f82f8e13b690010f8bfd7520735e6d37866685
faizkhan12 <faizk6797@gmail.com>
2021-11-22 22:53:15 +0530

    add fetching cell login in react application

--- packages/local-client/src/redux/action-creators/index.ts
+++ packages/local-client/src/redux/action-creators/index.ts
@@ -1,3 +1,4 @@
+import axios from "axios"
 import { Dispatch } from "redux"
 import bundle from "../../bundler"
 import { ActionType } from "../action-types"
@@ -9,7 +10,7 @@ import {
   MoveCellAction,
   UpdateCellAction,
 } from "../actions"
-import { CellTypes } from "../cell"
+import { Cell, CellTypes } from "../cell"
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -72,5 +73,26 @@ export const createBundle = (cellId: string, input: string) => {
         },
       },
     })
+  }
+}
+
+export const fetchCells = () => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.FETCH_CELLS,
+    })
+
+    try {
+      const { data }: { data: Cell[] } = await axios.get("/cells")
+      dispatch({
+        type: ActionType.FETCH_CELLS_COMPLETE,
+        payload: data,
+      })
+    } catch (error) {
+      dispatch({
+        type: ActionType.FETCH_CELLS_ERROR,
+        payload: error.message,
+      })
+    }
   }
 }
