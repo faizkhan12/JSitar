413fb9851adcbbb16c1a808c15d4889924dc9f04
faizkhan12 <faizk6797@gmail.com>
2022-11-12 21:31:04 +0530

    remove errors and .gitignore

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -1,47 +1,47 @@
-import express from "express"
-import fs from "fs/promises"
-import path from "path"
+import express from "express";
+import fs from "fs/promises";
+import path from "path";
 
 interface Cell {
-  id: string
-  content: string
-  type: "text" | "code"
+  id: string;
+  content: string;
+  type: "text" | "code";
 }
 
 export const createCellsRouter = (filename: string, dir: string) => {
-  const router = express.Router()
-  router.use(express.json())
+  const router = express.Router();
+  router.use(express.json());
 
-  const fullPath = path.join(dir, filename)
+  const fullPath = path.join(dir, filename);
 
   router.get("/cells", async (req, res) => {
     try {
       // read the file
-      const result = await fs.readFile(fullPath, { encoding: "utf8" })
-      res.send(JSON.parse(result))
-    } catch (err) {
+      const result = await fs.readFile(fullPath, { encoding: "utf8" });
+      res.send(JSON.parse(result));
+    } catch (err: any) {
       if (err.code === "ENOENT") {
         // add code to create a file and add default cells
-        await fs.writeFile(fullPath, "[]", "utf-8") // '[]' is a default cell for now
-        res.send([])
+        await fs.writeFile(fullPath, "[]", "utf-8"); // '[]' is a default cell for now
+        res.send([]);
       } else {
-        throw err
+        throw err;
       }
     }
     // parse a list of cells out of it
     // send list of cells back to browser
-  })
+  });
 
   router.post("/cells", async (req, res) => {
     // take the list of cells from the request obj and serialize them
-    const { cells }: { cells: Cell[] } = req.body
+    const { cells }: { cells: Cell[] } = req.body;
     // write the cells into the file
-    await fs.writeFile(fullPath, JSON.stringify(cells), "utf8")
+    await fs.writeFile(fullPath, JSON.stringify(cells), "utf8");
 
     res.send({
       status: "OK",
-    })
-  })
+    });
+  });
 
-  return router
-}
+  return router;
+};
